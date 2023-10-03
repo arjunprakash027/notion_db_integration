7934f9d88bc20b9a22b09e21657f166eb8f54769
Arjun P <arjunprakash027@gmail.com>
2023-10-03 17:00:19 +0530

    version 0.1.2

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "Readme.md").read_text()
+# from pathlib import Path
+# this_directory = Path(__file__).parent
+# long_description = (this_directory / "Readme.md").read_text()
 
 setup(
     name='notion_db_integration',
-    long_description=long_description,
+    long_description="to view tutorial and codebase click [here](https://github.com/arjunprakash027/notion_db_integration)",
     long_description_content_type='text/markdown',
-    version='0.1.1',
+    version='0.1.2',
     author='Arjun',
     author_email='arjunprakash027@gmail.com',
     description='A wrapper around Notion API to perform CRUD operations on your notion database',
